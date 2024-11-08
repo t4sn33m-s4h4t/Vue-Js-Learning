6d2de60c27a0e5d836d48d5b274291f403a4e22e
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2024-11-08 11:42:04 +0600

    learned

---
+++ Vue Learning/src/components/FoodItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+        <h1 @click="toggleFavorite">{{ foodName }}</h1>
+        
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        foodName: String,
+    },
+    
+    methods: {
+        toggleFavorite() {
+            
+            this.$emit('toggle-Favorite', this.foodName);
+        }
+    },
+}</script>
+
+<style></style>
